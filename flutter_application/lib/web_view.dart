09f5a86782c73063fc9819561fd6ac433147104b
dungntt <dungntt3@gmail.com>
2023-08-23 13:50:33 +0700

    test commit

--- flutter_application/lib/web_view.dart
+++ flutter_application/lib/web_view.dart
@@ -1,4 +1,6 @@
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application/web_view_plugin.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:flutter/services.dart';
@@ -26,106 +28,101 @@ class WebViewPage extends StatefulWidget {
 class _WebViewPageState extends State<WebViewPage> {
   late WebViewController _controller;
 
-  Uri addUriParameter(Uri uri, String key, String newValue) {
-    var params = uri.queryParameters;
-    var newUri = uri.replace(queryParameters: {});
-    params.forEach((key, value) {
-      newUri = newUri.replace(queryParameters: {key: value});
-    });
-    newUri.replace(queryParameters: {key: newValue});
-
-    return newUri;
-  }
-
-  static const platform = MethodChannel('nguyenvana');
-
-  Future<void> openBrowser(String url) async {
-    try {
-      print("ĐÃ VÀO ĐÂY RỒI");
-      final int result = await platform.invokeMethod('abc', {'url': url});
-      print("ĐÃ VÀO ĐÂY RỒI2");
-    } on PlatformException catch (e) {
-      // Unable to open the browser
-      print(e);
-    }
-  }
-
   @override
   void initState() {
     super.initState();
 
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (url) {},
-          onNavigationRequest: (NavigationRequest request) async {
-            print("Request: " + request.url);
-            if (request.url.startsWith('intent://')) {
-              // String requestUrl = request.url;
-              // int startIndex =
-              //     requestUrl.lastIndexOf('package=') + 'package='.length;
-              // int lastIndex = requestUrl.lastIndexOf(';');
-              // String packageApp = requestUrl.substring(startIndex, lastIndex);
+      ..setNavigationDelegate(CustomNavigationDelegate())
+      // ..setNavigationDelegate(
+      //   NavigationDelegate(
+      //     onNavigationRequest: (NavigationRequest request) async {
+      //       print("Request: " + request.url);
+      //       if (request.url.startsWith('intent://')) {
+      //         // String requestUrl = request.url;
+      //         // int startIndex =
+      //         //     requestUrl.lastIndexOf('package=') + 'package='.length;
+      //         // int lastIndex = requestUrl.lastIndexOf(';');
+      //         // String packageApp = requestUrl.substring(startIndex, lastIndex);
 
-              // bool isInstalled = await DeviceApps.isAppInstalled(packageApp);
-              // if (isInstalled) {
-              //   print('ĐÃ VÀO');
+      //         // bool isInstalled = await DeviceApps.isAppInstalled(packageApp);
+      //         // if (isInstalled) {
+      //         //   print('ĐÃ VÀO');
 
-              //   openBrowser(request.url);
+      //         //   openBrowser(request.url);
 
-              //   // Uri uri = Uri.parse(request.url);
-              //   // print('uri: $uri');
+      //         //   // Uri uri = Uri.parse(request.url);
+      //         //   // print('uri: $uri');
 
-              //   // try {
-              //   //   AndroidIntent intent = AndroidIntent(
-              //   //     action: 'action_view',
-              //   //     package: packageApp,
-              //   //     data:
-              //   //         "//view?data=000201010212262200069084050108VNPAY0015204123453037045405100005802VN5904VBAN6005HANOI626803082QXUI4J40519012305091357220328507082QXUI4J40817Thanh%20toan%20Qrcode63043CC2&callbackurl=https%3A%2F%2Fsandbox.vnpayment.vn%2Fpaymentv2%2Fqrback.html%3Ftoken%3Dcc165c8b5fc34965b841d1b277676537",
-              //   //     arguments: uri.queryParameters,
-              //   //   );
-              //   //   await intent.launch();
-              //   //   print(2);
-              //   // } catch (e) {
-              //   //   print(e);
-              //   // }
-              //   //
-              //   // print(intent.arguments);
+      //         //   // try {
+      //         //   //   AndroidIntent intent = AndroidIntent(
+      //         //   //     action: 'action_view',
+      //         //   //     package: packageApp,
+      //         //   //     data:
+      //         //   //         "//view?data=000201010212262200069084050108VNPAY0015204123453037045405100005802VN5904VBAN6005HANOI626803082QXUI4J40519012305091357220328507082QXUI4J40817Thanh%20toan%20Qrcode63043CC2&callbackurl=https%3A%2F%2Fsandbox.vnpayment.vn%2Fpaymentv2%2Fqrback.html%3Ftoken%3Dcc165c8b5fc34965b841d1b277676537",
+      //         //   //     arguments: uri.queryParameters,
+      //         //   //   );
+      //         //   //   await intent.launch();
+      //         //   //   print(2);
+      //         //   // } catch (e) {
+      //         //   //   print(e);
+      //         //   // }
+      //         //   //
+      //         //   // print(intent.arguments);
 
-              //   // try {
-              //   // Uri uri = Uri.parse(request.url);
-              //   // print('URI: $uri');
-              //   // print(uri.queryParametersAll);
+      //         //   // try {
+      //         //   // Uri uri = Uri.parse(request.url);
+      //         //   // print('URI: $uri');
+      //         //   // print(uri.queryParametersAll);
 
-              //   //   launchUrl(uri);
-              //   // } catch (e) {
-              //   //   print(e);
-              //   // }
-              //   // DeviceApps.openApp(packageApp);
-              //   // // AndroidIntent intent;
-              //   // // LaunchApp;
-              //   // Map data = {"Amount": "1200", "Operation": "operation"};
-              //   // AndroidIntent intent = AndroidIntent(
-              //   //   action: 'action_view',
-              //   //   data: data.toString(),
-              //   //   arguments: uri.queryParameters,
-              //   // );
-              //   // print(intent.arguments);
-              //   // await intent.launch();
-              //   // intent.launch();
-              // } else {
-              //   String urlString = "market://details?id=$packageApp";
-              //   Uri uri = Uri.parse(urlString);
-              //   launchUrl(uri);
-              // }
-              return NavigationDecision.navigate;
-            } else {
-              return NavigationDecision.navigate;
-            }
-          },
-        ),
-      )
+      //         //   //   launchUrl(uri);
+      //         //   // } catch (e) {
+      //         //   //   print(e);
+      //         //   // }
+      //         //   // DeviceApps.openApp(packageApp);
+      //         //   // // AndroidIntent intent;
+      //         //   // // LaunchApp;
+      //         //   // Map data = {"Amount": "1200", "Operation": "operation"};
+      //         //   // AndroidIntent intent = AndroidIntent(
+      //         //   //   action: 'action_view',
+      //         //   //   data: data.toString(),
+      //         //   //   arguments: uri.queryParameters,
+      //         //   // );
+      //         //   // print(intent.arguments);
+      //         //   // await intent.launch();
+      //         //   // intent.launch();
+      //         // } else {
+      //         //   String urlString = "market://details?id=$packageApp";
+      //         //   Uri uri = Uri.parse(urlString);
+      //         //   launchUrl(uri);
+      //         // }
+      //           // Uri uri = Uri.parse(request.url);
+      //           // if (await launchUrl(uri)) {
+      //           //   await launchUrl(uri);
+      //           // } else {
+      //           //   throw 'Could not launch $request.url';
+      //           // }
+      //           // const intent = AndroidIntent(
+      //           //     action: 'action_view',
+      //           //     data: 'intent://example.com',
+      //           //     package: 'com.android.chrome',
+      //           // );
+                
+      //           // intent.launch().catchError((e) {
+      //           //     launchUrl(Uri.parse('https://example.com'));
+      //           // });
+
+
+      //           return NavigationDecision.navigate;
+      //         // print("Request: " + request.url);
+      //         // return NavigationDecision.prevent;
+      //       } else {
+      //         return NavigationDecision.navigate;
+      //       }
+      //     },
+      //   ),
+      // )
       ..loadRequest(Uri.parse(widget.url));
   }
 
